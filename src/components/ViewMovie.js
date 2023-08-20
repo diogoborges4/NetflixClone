b4fdb249719de0660dfcabec605b1d9e82be5bb5
Diogo Borges <diogoborges9804@gmail.com>
2023-08-20 16:38:03 -0300

    first commit

---
+++ src/components/ViewMovie.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+import Tmdb from '../Tmdb';
+import "./ViewMovie.css"
+
+const ViewMovie = () => {
+    const { id } = useParams(); 
+
+    const [movieData, setMovieData] = useState(null)
+
+    useEffect(()=>{
+        const loadAll = async() =>{
+          let chosenInfo = await Tmdb.getMoiveInfo(id, 'tv')
+      
+          setMovieData(chosenInfo)
+        }
+      
+        loadAll()
+      }, [])
+
+    console.log(movieData)
+  return (
+    <div>
+        {movieData && movieData.name === "Warrior" &&
+            <div>
+                 <h1>{movieData.name}</h1>
+        <iframe className="videoo"src="https://embed.warezcdn.net/serie/tt5743796" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "The Witcher" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt5180504" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "A Guerra dos Tronos" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt0944947" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "Jujutsu Kaisen" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt12343534" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "O Eleito" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt1405354" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "Bleach" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt0434665" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "Futurama" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt0149460" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "마스크걸" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt26258200" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "Fundação" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt0804484" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "Invasão Secreta" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt13157618" frameborder="0"></iframe>
+            </div>
+        }
+        {movieData && movieData.name === "One Piece" &&
+            <div>
+                <h1>{movieData.name}</h1>
+                <iframe className='videoo' src="https://embed.warezcdn.net/serie/tt0388629" frameborder="0"></iframe>
+            </div>
+        }
+        
+    </div>
+  )
+}
+
+export default ViewMovie
