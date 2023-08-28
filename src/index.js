901a79f1f28af56efc481ae943fb9cc56fcbd25a
Diogo Borges <diogoborges9804@gmail.com>
2023-08-28 18:54:54 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ViewMovie from './components/ViewMovie.js';
-import Main from './main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route element={<Main/>}>
-      <Route  path={"/"} element={<App />}/>
-      <Route path={"/viewMovie/:id"} element={<ViewMovie />} />
-      </Route> 
+      <Route path="/" element={<App/>}/>
+      <Route path="/viewMovie/:id" element={<ViewMovie />} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
